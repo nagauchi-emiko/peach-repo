8025e8c2c231dc00c525fe8afd3b4109962feef6
Nagauchi Emiko <nagauchi.emiko@valuesccg.com>
2026-04-15 11:02:43 +0900

    firestoreへの設定値初期投入関数、投稿メッセージ登録関数を分離しました

--- update_firestore.py
+++ update_firestore.py
@@ -20,7 +20,7 @@ target_blocks = [
       "type": "section",
       "text": {
         "type": "mrkdwn",
-        "text": "*個別払請求書の登録はこちらから*"
+        "text": "*登録はこちらから*"
       }
     },
     {
@@ -90,7 +90,9 @@ target_blocks = [
           "type": "rich_text_section",
           "elements": [
             {"type": "text", "text": "※不明な点がある場合は、経理担当("},
-            {"type": "user", "user_id": "U05UPHBV0GK"},
+            {"type": "user", "user_id": "U05R8TB0XR6"},
+            {"type": "text", "text": " "},
+            {"type": "user", "user_id": "U01VD8QJC0G"},
             {"type": "text", "text": ")までお問い合わせください"}
           ]
         }
@@ -122,8 +124,24 @@ target_blocks = [
     }
 ]
 
-# 3. Firestoreへの保存実行
-def save_to_firestore():
+# Firestoreへの初期値の保存実行
+def save_to_firestore_initial_settings():
+
+    # 設定情報をFirestoreに保存（初回のみ）（既存のデータは上書きされます）
+    db.collection("settings").document("app_config").set({
+        "currencies": {"JPY": "日本円", "USD": "米国ドル", "EUR": "ユーロ", "CNY": "中国元", "THB": "タイバーツ"},
+        "accounting_users": ["U05UPHBV0GK", "U05R8TB0XR6", "U01VD8QJC0G"],  # 長内、近藤、藤井裕菜
+        "system_admin_members": ["U05UPHBV0GK"],  # 長内
+        "scopes": ["drive","spreadsheets"],
+        "google_drive_folder_id": "1rZRibqd7q4OXGM7NrdfyfUgriA_gdfKo", # 経理共有ドライブのルートフォルダID(これはダミーデータです)
+        "redirect_uri": "https://peach-app-237986654776.asia-northeast1.run.app/auth/google/callback" # OAuthのリダイレクトURI
+    })
+    
+    db.collection("folders_info").document("exclude_folder_ids").set({
+        "exclude_folder_ids": ["1n9j8sjvYlLZtXUu9qLh8nXoLm3mN1", "1sKjv0a2eHqfVh5r7wW8x9yZcD4e5f6"]}) # 除外するフォルダIDの例
+
+# Firestoreへ投稿メッセージの保存実行
+def save_to_firestore_post_message():
     doc_ref = db.collection("settings").document("invoice_setup_message")
     
     # データをセット（既存のデータは上書きされます）
@@ -133,18 +151,6 @@ def save_to_firestore():
     print("Successfully updated Firestore: settings/invoice_setup_message")
 
 
-    # 設定情報をFirestoreに保存（初回のみ）（既存のデータは上書きされます）
-    db.collection("settings").document("app_config").set({
-        "currencies": {"JPY": "日本円", "USD": "米国ドル", "EUR": "ユーロ", "CNY": "中国元", "THB": "タイバーツ"},
-        "accounting_users": ["U05UPHBV0GK", "U05R8TB0XR6", "U01VD8QJC0G"],  # 長内、近藤、藤井裕菜
-        "system_admin_members": ["U05UPHBV0GK"],  # 長内
-        "scopes": ["drive","spreadsheets"],
-        "google_drive_folder_id": "1rZRibqd7q4OXGM7NrdfyfUgriA_gdfKo", # 経理共有ドライブのルートフォルダID
-        "redirect_uri": "https://peach-app-237986654776.asia-northeast1.run.app/auth/google/callback" # OAuthのリダイレクトURI
-    })
-    
-    db.collection("folders_info").document("exclude_folder_ids").set({
-        "exclude_folder_ids": ["1n9j8sjvYlLZtXUu9qLh8nXoLm3mN1", "1sKjv0a2eHqfVh5r7wW8x9yZcD4e5f6"]}) # 除外するフォルダIDの例
-
 if __name__ == "__main__":
-    save_to_firestore()
+    # save_to_firestore_initial_settings()
+    save_to_firestore_post_message()
