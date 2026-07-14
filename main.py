2a300bad74ae505daf515b9bea03b85dc3544203
Nagauchi Emiko <nagauchi.emiko@valuesccg.com>
2026-07-14 11:10:57 +0900

    Googleのスコープエラーへの対応

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ FastAPI + Slack Bolt メインアプリケーション
 HTTP モード（Socket Mode 不使用）で Slack イベントを処理
 """
 import os
+# Googleから余計なスコープ（emailやprofileなど）が戻ってきてもエラーにしない設定
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "1"
+
 import logging
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
